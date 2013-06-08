259d6c03be5c74d296ed3961f2c83617129ae85d
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-06-08 16:06:28 +0900

    Import assets.

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -4,12 +4,18 @@ using System.Collections;
 public class Hit : MonoBehaviour {
 	public float limitVel = 2.0f;
 	public float oldValue = 0.0f;
+	
+	public GameObject hitEffect = null;
 
 	void OnCollisionStay(Collision collisionInfo)
 	{
 		float diff = Mathf.Abs(oldValue - collisionInfo.relativeVelocity.magnitude);
 		if(diff > limitVel) {
 			Destroy(this.gameObject);
+			Instantiate(
+				hitEffect,
+				this.transform.position,
+				this.transform.rotation);
 		}
 		oldValue = collisionInfo.relativeVelocity.magnitude;
 	}
